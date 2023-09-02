ef972031041b6df4f481be47d6492ab8c427b9d6
Pervejhasan <mdpervej12441@gmail.com>
2023-09-03 00:37:04 +0600

    use  middleware redux-thunk and axios

---
+++ redux-thunk_middleware.js
@@ -0,0 +1,92 @@
+//async actions - api calling
+//api url - https://jsonplaceholder.typicode.com/todos
+//middleware - redux-thunk
+//axios api
+
+
+const { default: axios } = require("axios");
+const { createStore,applyMiddleware } = require("redux");
+const thunk=require('redux-thunk').default;
+//constants
+const GET_TODOS_REQUEST="GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS="GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED="GET_TODOS_FAILED";
+const API_URL=" https://jsonplaceholder.typicode.com/todos";
+// state
+const initialTodoState={
+    todos: [],
+    isLoading: false, 
+    error:null
+}
+
+//action
+const getTodosRequest=()=>{
+    return{
+        type:GET_TODOS_REQUEST
+    }
+}
+const getTodosSuccess=(todos)=>{
+    return{
+        type:GET_TODOS_SUCCESS,
+        payload:todos
+
+    }
+}
+const getTodosFailed=(error)=>{
+    return{
+        type:GET_TODOS_FAILED,
+        payload:error
+    }
+}
+
+
+//reducer
+const todosReducer=(state=initialTodoState,action)=>{
+
+switch(action.type){
+    case GET_TODOS_REQUEST:
+        return{
+            ...state,
+            isLoading:true
+        }
+        case GET_TODOS_SUCCESS:
+        return{
+            ...state,
+            isLoading:false,
+            todos:action.payload
+        }
+        case GET_TODOS_FAILED:
+        return{
+            ...state,
+            isLoading:false,
+            error:action.payload
+        }
+        default:
+            return state;
+}
+}
+
+
+//async action creator
+const fetchData =()=> {
+return (dispatch)=>{
+dispatch(getTodosRequest());
+axios.get(API_URL)
+.then((res)=>{
+const todos=res.data;
+const titles=todos.map(todo=>todo.title);
+dispatch(getTodosSuccess(titles))
+
+}).catch((error)=>{
+    dispatch(getTodosFailed(error.message))
+})
+}
+}
+
+//store
+const store=createStore(todosReducer,applyMiddleware(thunk));
+store.subscribe(()=>{
+   console.log( store.getState())
+})
+
+store.dispatch(fetchData())
